1a7d15ae49262686edc9f185e01ab8a80d8ada6b
qingzhixing <qzsqqmail@qq.com>
2024-11-10 22:52:32 +0800

    Initialize

---
+++ src/client/java/qingzhixing/weird_items/client/Weird_itemsClient.java
@@ -0,0 +1,10 @@
+package qingzhixing.weird_items.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Weird_itemsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/qingzhixing/weird_items/Weird_items.java
@@ -0,0 +1,10 @@
+package qingzhixing.weird_items;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Weird_items implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
