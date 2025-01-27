2102f3b5164ce0a41eb9d5380b248c41b56417fb
qingzhixing <qzsqqmail@qq.com>
2025-01-27 12:29:51 +0800

    🩲 更改项目结构

--- src/main/java/qingzhixing/weird_items/mixin/EnchantmentMixin/InfinityEnchantmentBucketMixin.java
+++ src/main/java/qingzhixing/weird_items/mixin/EnchantmentMixin/InfinityEnchantmentBucketMixin.java
@@ -1,7 +1,8 @@
-package qingzhixing.weird_items.mixin;
+package qingzhixing.weird_items.mixin.EnchantmentMixin;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.InfinityEnchantment;
+import net.minecraft.item.EntityBucketItem;
 import net.minecraft.item.FluidModificationItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,19 +10,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Arrays;
-
 @Mixin(InfinityEnchantment.class)
 public class InfinityEnchantmentBucketMixin extends EnchantmentMixin {
     @Override
     protected void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         var stackItem = stack.getItem();
-        Arrays.stream(stackItem.getClass().getInterfaces()).forEach(interfaceClass -> {
-                    if (interfaceClass == FluidModificationItem.class) {
-                        cir.setReturnValue(true);
-                    }
-                }
-        );
+        if (stackItem instanceof FluidModificationItem && !(stackItem instanceof EntityBucketItem)) {
+            cir.setReturnValue(true);
+        }
     }
 }
 
